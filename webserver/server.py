d5a5b3dcd8026ff7a2a1015147fa03c347d8f724
juneiris <xcw930620@163.com>
2016-03-22 16:00:50 -0400

    try query shops

--- webserver/server.py
+++ webserver/server.py
@@ -276,6 +276,7 @@ def restlist():
       #cur = g.conn.execute('SELECT s.shopname FROM shops s,locate_in l WHERE s.shopid=l.shopid AND s.shoptype=type AND l.postcode=area AND s.s_takeout=take_out AND s.s_delivery=delievery')
       #q = 'SELECT s.shopname FROM shops s WHERE s.shoptype=%s AND s.s_takeout=%s'
       q="SELECT s.shopname FROM shops s"+l+w+stp+alian+sa+tlian+stake++slian+sd
+      print q
       cur = g.conn.execute(q)
       #cur = g.conn.execute(q,type,take_out)
 
