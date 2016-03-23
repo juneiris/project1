ad4fc6bc5734ffe6fee095a0579480cddf0f76bb
juneiris <xcw930620@163.com>
2016-03-23 11:36:06 -0400

    try query shops

---
+++ webserver副本/server.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python2.7
+
+"""
+Columbia W4111 Intro to databases
+Example webserver
+
+To run locally
+
+    python server.py
+
+Go to http://localhost:8111 in your browser
+
+
+A debugger such as "pdb" may be helpful for debugging.
+Read about it online.
+"""
+
+import os
+from datetime import datetime
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+
+# configuration
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+#
+# The following uses the sqlite3 database test.db -- you can use this for debugging purposes
+# However for the project you will need to connect to your Part 2 database in order to use the
+# data
+#
+# XXX: The URI should be in the format of: 
+#
+#     postgresql://USER:PASSWORD@w4111db.eastus.cloudapp.azure.com/username
+#
+# For example, if you had username ewu2493, password foobar, then the following line would be:
+#
+#     DATABASEURI = "postgresql://ewu2493:foobar@w4111db.eastus.cloudapp.azure.com/ewu2493"
+#
+#DATABASEURI = "sqlite:///test.db"
+
+DATABASEURI = "postgresql://cx2178:RDATHT@w4111db.eastus.cloudapp.azure.com/cx2178"
+
+
+
+#
+# This line creates a database engine that knows how to connect to the URI above
+#
+engine = create_engine(DATABASEURI)
+
+
+#
+# START SQLITE SETUP CODE
+#
+# after these statements run, you should see a file test.db in your webserver/ directory
+# this is a sqlite database that you can query like psql typing in the shell command line:
+# 
+#     sqlite3 test.db
+#
+# The following sqlite3 commands may be useful:
+# 
+#     .tables               -- will list the tables in the database
+#     .schema <tablename>   -- print CREATE TABLE statement for table
+# 
+# The setup code should be deleted once you switch to using the Part 2 postgresql database
+#
+#engine.execute("""DROP TABLE IF EXISTS test;""")
+#engine.execute("""CREATE TABLE IF NOT EXISTS test (
+#  id serial,
+#  name text
+#);""")
+#engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+#
+# END SQLITE SETUP CODE
+#
+
+uid = '111111'
+
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request
+
+  The variable g is globally accessible
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print "uh oh, problem connecting to database"
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+
+#
+# @app.route is a decorator around index() that means:
+#   run index() whenever the user tries to access the "/" path using a GET request
+#
+# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
+#
+#       @app.route("/foobar/", methods=["POST", "GET"])
+#
+# PROTIP: (the trailing / in the path is important)
+# 
+# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
+# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
+#
+@app.route('/')
+def index():
+  """
+  request is a special object that Flask provides to access web request information:
+
+  request.method:   "GET" or "POST"
+  request.form:     if the browser submitted a form, this contains the data in the form
+  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+
+  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+  """
+
+  # DEBUG: this is debugging code to see what request looks like
+  print request.args
+
+
+  #
+  # example of a database query
+  #
+
+
+  #cursor = g.conn.execute("SELECT * FROM users")
+  #names = []
+  #for result in cursor:
+    #names.append(result['username'])  # can also be accessed using result[0]
+  #cursor.close()
+
+  #
+  # Flask uses Jinja templates, which is an extension to HTML where you can
+  # pass data to a template and dynamically generate HTML based on the data
+  # (you can think of it as simple PHP)
+  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
+  #
+  # You can see an example template in templates/index.html
+  #
+  # context are the variables that are passed to the template.
+  # for example, "data" key in the context variable defined below will be 
+  # accessible as a variable in index.html:
+  #
+  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
+  #     <div>{{data}}</div>
+  #     
+  #     # creates a <div> tag for each element in data
+  #     # will print: 
+  #     #
+  #     #   <div>grace hopper</div>
+  #     #   <div>alan turing</div>
+  #     #   <div>ada lovelace</div>
+  #     #
+  #     {% for n in data %}
+  #     <div>{{n}}</div>
+  #     {% endfor %}
+  #
+  #context = dict(data = names)
+
+
+  #
+  # render_template looks in the templates/ folder for files.
+  # for example, the below file reads template/index.html
+  #
+  return render_template("index.html")
+
+#
+# This is an example of a different path.  You can see it at
+# 
+#     localhost:8111/another
+#
+# notice that the functio name is another() rather than index()
+# the functions for each app.route needs to have different names
+#
+@app.route('/another')
+def another():
+  return render_template("anotherfile.html")
+
+# @app.route('/login')
+# def login():
+#     return render_template("login.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+       username = request.form['username']
+       passwords = g.conn.execute("SELECT password FROM users WHERE username='%s'"%username)
+       uids = g.conn.execute("SELECT userid FROM users WHERE username='%s'"%username)
+       for id in uids:
+           uid = id[0]
+	   print uid
+       for password in passwords:
+	   print password[0]
+           if request.form['password'] != password[0]:
+                error = 'Login Error, Try Again'
+           else:
+#               session['logged_in'] = True
+               print 'You were logged in'
+               return redirect('/')
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect('index.html')
+
+@app.route('/createaccount')
+def create_account():
+    return render_template("createaccount.html")
+
+
+# Example of adding new data to the database
+@app.route('/add', methods=['POST'])
+def add():
+  name = request.form['name']
+  g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
+  return redirect('/')
+
+
+#show restuarants according to filter
+@app.route('/restlist', methods=['POST'])
+def restlist():
+  type = request.form['Type']
+  area = request.form['Area']
+  take_out = request.form['Take_out']
+  delivery = request.form['Delivery']
+  sort=request.form['Sort']
+  if sort=="none":
+      ratingsort=""
+  if sort=="DESC":
+      ratingsort=" DESC"
+
+  if type=="none" and area=="none" and take_out=="none" and delivery=="none":
+      q="SELECT shopname,rating_score FROM shops ORDER BY rating_score"+ratingsort
+      print q
+      cur = g.conn.execute(q)
+  else:
+      w=" WHERE"
+      if type=="none":
+          stp=""
+      else:
+          stp=" s.shoptype='%s'"%type
+
+      if area=="none":
+          sa=""
+          l=""
+          alian=""
+      else:
+          alian=""
+          sa=" l.shopid=s.shopid AND l.postcode='%s'"%area
+          l=",locate_in l"
+          if type!="none":
+              alian=" AND"
+
+      if take_out=="none":
+          stake=""
+          tlian=""
+      else:
+          tlian=""
+          stake=" s.s_takeout='%s'"%take_out
+          if type!="none" or area!="none":
+              tlian=" AND"
+
+      if delivery=="none":
+          sd=""
+          slian=""
+      else:
+          slian=""
+          sd=" s.s_delivery='%s'"%delivery
+          if type!="none" or area!="none" or take_out!="none":
+              slian=" AND"
+
+      #cur = g.conn.execute('SELECT s.shopname FROM shops s,locate_in l WHERE s.shopid=l.shopid AND s.shoptype=type AND l.postcode=area AND s.s_takeout=take_out AND s.s_delivery=delievery')
+      #q = 'SELECT s.shopname FROM shops s WHERE s.shoptype=%s AND s.s_takeout=%s'
+      q="SELECT s.shopname,s.rating_score FROM shops s"+l+w+stp+alian+sa+tlian+stake+slian+sd+" ORDER BY s.rating_score"+ratingsort
+      #q="SELECT s.shopname FROM shops s"+l+w+stp+alian+sa
+      #q="SELECT s.shopname FROM shops s"+l+w+stp+tlian+stake
+
+      print q
+      cur = g.conn.execute(q)
+      #cur = g.conn.execute(q,type,take_out)
+
+  names = []
+  #rating=[]
+  print cur
+  for result in cur:
+      names.append(result[0]+"     "+str(result[1]))  # can also be accessed using result[0]
+      #rating.append(result[1])
+  cur.close()
+
+  context = dict(data = names)
+  #g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
+  return render_template("index.html", **context)
+
+
+
+
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+
+        python server.py
+
+    Show the help text using
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
