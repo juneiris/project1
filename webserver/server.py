ccc0e73c50c86b52956440ef9fd8acd4a09f4617
juneiris <xcw930620@163.com>
2016-03-26 22:05:18 -0400

    try like

--- webserver/server.py
+++ webserver/server.py
@@ -237,9 +237,9 @@ def another():
                 error='Please login first'
                 return render_template('anotherfile.html', error=error)
             else:
-                q="SELECT u.username FROM likes l, users u WHERE u.userid<>'%s' AND u.userid=l.userid AND shopid='%s'"%uid%shopid
+                q="SELECT u.username FROM likes l, users u WHERE u.userid<>'%s' AND u.userid=l.userid AND shopid='%s'"
                 print q
-                lpeople=g.conn.execute(q)
+                lpeople=g.conn.execute(q,(uid,shopid))
                 people=[]
                 for result in lpeople:
                     print result[0]
